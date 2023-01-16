43f812dd6b41cbf8599549a5ec16b73ed719dc04
choi <wjfekr1@naver.com>
2023-01-17 02:47:39 +0900

    이모지 페이지에 MBTI 연결

--- static/js/emoji.js
+++ static/js/emoji.js
@@ -1,6 +1,19 @@
-const imgBox = document.querySelector("imgBox");
+// const imgBox = document.querySelector("imgBox");
 
-imgBox.addEventListener("click", function () {
-  let clickedCard = e.target;
-  clickedCard.classList.add("flip");
-});
+// imgBox.addEventListener("click", function () {
+//   let clickedCard = e.target;
+//   clickedCard.classList.add("flip");
+// });
+
+// 해당하는 MBTI값을 리턴합니다.
+const getMBTI = () => {
+  let a = new URL(window.location.href);
+  let b = a.searchParams;
+  let c = b.get("mbti");
+
+  return c;
+};
+let MBTI = getMBTI();
+
+// 테스트 용입니다.
+document.getElementById("test").innerHTML = `<h1>${MBTI}</h1>`;
