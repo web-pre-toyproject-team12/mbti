b4992ff0864aad2cd01ef5165b8aaa66eb967273
GIBeom-Cheon <110073263+GIBeom-Cheon@users.noreply.github.com>
2023-01-16 23:03:48 +0900

    Feat: first commit

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.g596pjc.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+##회원가입
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    nickname_receive = request.form['nickname_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest() # password 해쉬화 함수
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+    }
+    db.users.insert_one(doc) 
+    return jsonify({'result': 'success'})
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,4 +32,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
